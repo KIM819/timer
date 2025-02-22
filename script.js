78fa8fa3814f4021a3857bef4a5da97cb4091245
KIM819 <kimuriku0714@rakumail.jp>
2025-02-22 20:22:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -151,9 +151,7 @@ document.addEventListener("DOMContentLoaded", function () {
     updateDisplay();
 });
 
-body.cycle-rest() {
-    background-color: #6699ff;
-}
+
 window.onload = function () {
     console.log("JavaScript がロードされました");
 
